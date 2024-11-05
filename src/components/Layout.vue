4755f377ae9f493b1ded29cd03b4f1c33a43f9a5
chihuahua <chihuahua@babydoge.com>
2024-11-05 13:15:33 +0300

    feat: add floating elements

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,15 +1,36 @@
 <script setup lang="ts">
 import Header from "../components/nav/Header.vue";
 import Footer from "../components/nav/Footer.vue";
+import { useWindowScroll } from "../composables/useWindowScroll";
+import FloatingItem from "./base/FloatingItem.vue";
+
+const { y } = useWindowScroll();
+
+const handleScrollDown = () => {
+  console.log("scrolling");
+  window.scrollBy({ top: window.innerHeight, left: 0, behavior: "smooth" });
+};
 </script>
 
 <template>
-  <div class="min-h-screen w-full flex flex-col max-w-[1440px] mx-auto">
-    <Header></Header>
-    <main class="flex-grow mt-[90px]">
+  <div class="min-h-screen w-full flex flex-col">
+    <Transition name="fade-in">
+      <Header v-if="y > 100" class="max-w-[1440px] mx-auto"></Header>
+    </Transition>
+
+    <FloatingItem class="fixed bottom-[100px] left-[50%]">
+      <button
+        class="border-own-white border-[2px] w-10 h-10 rounded-full flex items-center justify-center"
+        @click="handleScrollDown"
+      >
+        <i class="material-icons">arrow_downward</i>
+      </button>
+    </FloatingItem>
+
+    <main class="flex-grow">
       <slot />
     </main>
-    <Footer></Footer>
+    <Footer class="max-w-[1440px] mx-auto"></Footer>
   </div>
 </template>
 
---
+++ src/components/base/FloatingItem.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="floating-container w-min h-min">
+    <div class="floating">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.floating {
+  animation: floating 2s ease-in-out infinite;
+}
+
+.floating-container {
+  /* animation: container-appear 2s cubic-bezier(0.48, 0.56, 0.49, 1.95) forwards; */
+  transform-origin: center;
+}
+
+@keyframes container-appear {
+  from {
+    opacity: 0;
+    transform: scale(0.5);
+  }
+  to {
+    opacity: 1;
+    transform: scale(1);
+  }
+}
+
+@keyframes floating {
+  from {
+    -webkit-transform: translate(0, 0px);
+  }
+  65% {
+    -webkit-transform: translate(0, 5px);
+  }
+  to {
+    -webkit-transform: translate(0, -0px);
+  }
+}
+</style>
